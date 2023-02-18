9cdcbdb0bc27df1287fb13489cc234d1df4278ca
Licho <lecho.sun@gmail.com>
2023-02-18 14:14:38 +0800

    refactor: dot to t function name
    Signed-off-by: Licho <lecho.sun@gmail.com>

--- tests/dataframeFunction.py
+++ tests/dataframeFunction.py
@@ -6,23 +6,23 @@ import numpy as np
 import pandas as pd
 
 
-def dot(self: Any, name: str = None, *args, **kwargs):
+def t(self: Any, name: str = '', *args, **kwargs):
     if getattr(self, name, False):
         # contains name
         result = getattr(self, name)(*args, **kwargs)
-        if getattr(result, dot.__name__, True):
+        if getattr(result, t.__name__, True):
             if isinstance(self, types.ModuleType):
-                self.dot = functools.partial(dot, self)
+                self.t = functools.partial(t, self)
             else:
-                result.dot = types.MethodType(dot, result)
+                result.t = types.MethodType(t, result)
         return result
 
-    if getattr(self, dot.__name__, True):
-        # no func attribute
+    if getattr(self, t.__name__, True):
+        # no dot attribute
         if isinstance(self, types.ModuleType):
-            self.dot = functools.partial(dot, self)
+            self.t = functools.partial(t, self)
         else:
-            self.dot = types.MethodType(dot, self)
+            self.t = types.MethodType(t, self)
         return self
 
 
@@ -32,15 +32,15 @@ def testDataFrame(n):
                                             freq="W-WED"),
                         columns=["Colorado", "Texas", "New York", "Ohio"])
     df = pd.DataFrame(data)
-    print(dot(df, "").dot('resample', 'D').dot('asfreq'))
+    print(t(df).t('resample', 'D').t('asfreq'))
 
 
 def test2():
-    a = dot(np.random, 'standard_normal', (2, 4))
-    index = dot(pd, 'date_range', '2000-01-01', periods=2, freq="W-WED")
-    frame = dot(pd, "DataFrame", a,
-                index=index,
-                columns=["Colorado", "Texas", "New York", "Ohio"])
+    a = t(np.random, 'standard_normal', (2, 4))
+    index = t(pd, 'date_range', '2000-01-01', periods=2, freq="W-WED")
+    frame = t(pd, "DataFrame", a,
+              index=index,
+              columns=["Colorado", "Texas", "New York", "Ohio"])
     print(frame)
 
 
